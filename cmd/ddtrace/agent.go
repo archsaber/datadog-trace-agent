0b42d7110aacd334d43abe3f723ebeadddcd3e7e
Arpit Jain <arjain@sumologic.com>
2019-10-06 23:23:32 +0530

    skip check for trace being too far in past

--- cmd/ddtrace/agent.go
+++ cmd/ddtrace/agent.go
@@ -209,13 +209,13 @@ func (a *Agent) Process(t model.Trace) {
 	}
 	atomic.AddInt64(priorityPtr, 1)
 
-	if root.End() < model.Now()-2*a.conf.BucketInterval.Nanoseconds() {
+	/*if root.End() < model.Now()-2*a.conf.BucketInterval.Nanoseconds() {
 		log.Errorf("skipping trace with root too far in past, root:%v", *root)
 
 		atomic.AddInt64(&ts.TracesDropped, 1)
 		atomic.AddInt64(&ts.SpansDropped, int64(len(t)))
 		return
-	}
+	}*/
 
 	for _, f := range a.Filters {
 		if f.Keep(root, &t) {
